83a7d2620c5229847837494dd84e69fe6f3649d8
louiselaux <louise.laux@imev-mer.fr>
2025-03-24 10:17:07 +0100

    Update 1.HPLC_trends.R
    add HPLC data to main

--- codes/2.analyses/1.HPLC_trends.R
+++ codes/2.analyses/1.HPLC_trends.R
@@ -27,9 +27,9 @@ hplc_trend%>%mutate(year=year(date))%>%group_by(year)%>%filter(year=="2015")
 
 #####Regularize the time serie #####
 
-## Regularise series ----
+## Regularise series
 
-# define a 2 weeks regular date grid
+# Define a 2 weeks regular date grid
 ref <- tibble(
   target_date=seq(from=as.Date("2012-01-05"), to=as.Date("2022-12-31"), by=14),
   year=year(target_date)
@@ -37,19 +37,19 @@ ref <- tibble(
 
 
 # identify years in which the number of obs is larger than usual
-pbs <- ref |>
-  count(year) |>
+pbs <- ref %>%
+  count(year) %>%
   filter(n>26)
 # ->this is often an extra date in very late decembre => just remove it
 ref <- filter(ref, !(year %in% pbs$year & yday(target_date) > 360))
-ref |>
-  count(year) |>
+ref %>%
+  count(year) %>%
   filter(n>26)
 # -> all OK
 
 # match data based on these reference dates
 avail_dates <- unique(hplc_trend$date)
-ref <- ref |>
+ref <- ref %>%
   mutate(
     closest_date = castr::closest(ref$target_date, avail_dates),
     date_diff = abs(closest_date - target_date) |> as.numeric()
@@ -76,37 +76,41 @@ hplct%>%filter(name %in% c("diatoms","dinoflagellates","green_algae","micro_chla
   geom_point(aes(x=target_date, y=value), size=0.2) + theme_bw()+labs(x="date", y="valeur") +  theme(
     strip.background = element_rect(fill = "lightblue", color = "darkblue"),
     strip.text = element_text(color = "black", face = "bold")
-  )
+  )+ scale_y_log10()
 
 hplct%>%mutate(month=factor(month(target_date)))%>%filter(name=="diatoms")%>%group_by(month,year)%>%summarise(count=n()) # 2 per month so ok
 
+
+#Some plots
 hplctt<- hplct
 hplctt$week<- factor(week(hplctt$target_date))
+
+#Seasonal variations of diatoms
 hplctt%>%filter(name=="diatoms")%>%ggplot(aes(x = year, y = week, fill = value)) +
-  geom_tile(color = "white") +  # Créer le heatmap
-  # Palette de couleurs pour les valeurs
+  geom_tile(color = "white") +
+
   labs(x = "Année", y = "Semaine", fill = "Valeur") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   scale_fill_gradientn(
     colors = c("lightblue", "red"),
-    limits = c(0.0, 2)  # Limites des valeurs
+    limits = c(0.0, 2)
   )+
   #scale_y_discrete(labels = c("Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
   #                           "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"))+
   scale_x_continuous(breaks = seq(min(hplctt$year), max(hplctt$year), by = 5))+
   facet_wrap(~name, scales = "free_y")
 
-
+#Seasonal variations of dinoflagellates
 hplctt%>%filter(name=="dinoflagellates")%>%ggplot(aes(x = year, y = week, fill = value)) +
-  geom_tile(color = "white") +  # Créer le heatmap
-  # Palette de couleurs pour les valeurs
+  geom_tile(color = "white") +
+
   labs(x = "Année", y = "Semaine", fill = "Valeur") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   scale_fill_gradientn(
     colors = c("lightblue", "red"),
-    limits = c(0.0, 0.5)  # Limites des valeurs
+    limits = c(0.0, 0.5)
   )+
   #scale_y_discrete(labels = c("Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
   #                           "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"))+
@@ -115,7 +119,7 @@ hplctt%>%filter(name=="dinoflagellates")%>%ggplot(aes(x = year, y = week, fill =
 
 hplctt%>%filter(name=="prokaryotes")%>%ggplot()+geom_point(aes(x=as.factor(month),y=as.numeric(value), color=as.factor(year)))
 
-#####Dsitribution of the different variables#####
+#####Distribution of the different variables#####
 variables <- unique(hplct$name)
 
 ggplot(hplct, aes(x = value)) +
@@ -128,11 +132,11 @@ ggplot(hplct, aes(x = value)) +
     y = "Frequency"
   )
 
-#Transformation before
+#Log transformation before
 hplct_log <- hplct %>%
-  mutate(value_log = ifelse(value > 0, log(value), NA))
+  mutate(value_log = log10(value))
 
-# Créer l'histogramme pour les valeurs transformées
+# See hist
 ggplot(hplct_log, aes(x = value_log)) +
   geom_histogram(binwidth = 0.2, fill = "blue", color = "white", alpha = 0.7) +
   facet_wrap(~name, scales = "free", ncol = 2) +
@@ -163,7 +167,7 @@ dstl_hplc <- hplct |> filter(name %in% c("diatoms","dinoflagellates","nano_chla"
     out <- bind_cols(select(x, target_date:date_diff), out)
   }) |>
   ungroup() |>
-  
+
   # cut the part before the variable becomes available for the first time
   group_by(name) |>
   group_modify(.f=function(x, y) {
@@ -195,26 +199,26 @@ library("nlme")
 
 glance.gls <- function(m) {
   s <- summary(m)
-  
+
   # r.squared
   f <- predict(m)
   mss <- sum((f - mean(f))^2)
   rss <- sum(residuals(m)^2)
   rsq <- mss / (mss + rss)
-  
+
   # residuals
   shap <- shapiro.test(m$residuals)
   a <- pacf(residuals(m, type="normalized"), plot=FALSE)
-  
+
   tibble(
     r.squared = rsq,
-    
+
     statistic = s$tTable[2, "t-value"],
     p.value = s$tTable[2, "p-value"],
-    
+
     intercept = m$coefficients[1],
     slope = m$coefficients[2],
-    
+
     shapiro.p.value = shap$p.value,
     cor.struct=class(m$modelStruct$corStruct)[1],
     acf1 = a$acf[1],
@@ -234,19 +238,19 @@ stats_hplc <- dstl_hplc |>filter(name %in% c("diatoms","dinoflagellates","micro_
     # 0. fill missing values through linear interpolation
     x$deseason_filled <- castr::interpolate(x=x$target_date, y=x$deseason, xout=x$target_date)
     # TODO check length of "holes"
-    
+
     #Where are the NA in deseason
     #is_na <- is.na(x$deseason)
-    
+
     #Length
     #na_runs <- rle(is_na)
     #na_lengths <- na_runs$lengths[na_runs$values == TRUE]
     #x$na_gap_length <- ifelse(is_na, na_lengths[1], 0)
-    
+
     # return(x)
     # 1. Mann-Kendall trend test
     mkt <- trend::mk.test(x$deseason_filled)
-    
+
     # 2. GLS regression
     # simple model
     m <- gls(deseason_filled ~ target_date, data=x)
@@ -256,22 +260,10 @@ stats_hplc <- dstl_hplc |>filter(name %in% c("diatoms","dinoflagellates","micro_
       # add AR1 model on residuals
       m <- gls(deseason_filled ~ target_date, data=x, cor=corAR1(round(a$acf[1], 1)))
       a <- pacf(residuals(m, type="normalized"), plot=FALSE)
-      
-      # TODO investigate if any of this is necessary here
-      # # if autocorrelation is still too strong
-      # # NB: does not bring a much better fit and is very long
-      # if (abs(a$acf[1]) > 0.2) {
-      #   # add AR2 model on residuals
-      #   cor <- round(a$acf[1:2], 1)
-      #   # stabilise errors if the autocorr is too strong
-      #   if (sum(cor) > 0.9){ cor <- cor - 0.1 }
-      #   m <- gls(deseason ~ date, data=x, cor=corARMA(cor, form=~date, p=2, q=0))
-      #   # a <- pacf(residuals(m, type="normalized"), plot=FALSE)
-      # }
-      
-      
+
+
     }
-    
+
     # extract diagnostic information for both approaches
     bind_cols(
       glance(mkt) |> select(mk_p.value=p.value),
@@ -318,13 +310,15 @@ ggplot() +
   ggtitle("Tendances à long terme") +
   theme_bw() +
   theme(
-    axis.title = element_text(size = 14),        # Taille des titres des axes
-    axis.text = element_text(size = 12),         # Taille des textes des axes
-    strip.text = element_text(size = 16),        # Taille des titres des facettes
-    legend.title = element_text(size = 14),      # Taille du titre de la légende
-    legend.text = element_text(size = 12),       # Taille du texte de la légende
-    plot.title = element_text(size = 18, hjust = 0.5),  # Taille du titre principal
+    axis.title = element_text(size = 14),
+    axis.text = element_text(size = 12),
+    strip.text = element_text(size = 16),
+    legend.title = element_text(size = 14),
+    legend.text = element_text(size = 12),
+    plot.title = element_text(size = 18, hjust = 0.5),
     axis.ticks.length = unit(0.3, "cm"),
-    strip.background = element_rect(fill = "lightblue")# Taille des ticks des axes
-  )
+    strip.background = element_rect(fill = "lightblue")
+
+
+    #test
 
